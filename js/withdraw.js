e2bc8393b569fee27597cc7c3dc57910ca91eb29
wahidvai <ferdaouswahid2001@gmail.com>
2022-08-18 00:23:30 +0600

    simple way to code bap er bank

---
+++ js/withdraw.js
@@ -0,0 +1,21 @@
+document.getElementById('btn-withdraw').addEventListener('click', function () {
+  const withdrawField = document.getElementById('withdraw-field');
+  const newWithdrawAmountString = withdrawField.value;
+  const newWithdrawAMount = parseFloat(newWithdrawAmountString);
+
+  withdrawField.value = '';
+
+  const withdrawTotalElement = document.getElementById('withdraw-total');
+  const previousWithdrawTotalString = withdrawTotalElement.innerText;
+  const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
+
+  const newWithdrawTotal = previousWithdrawTotal + newWithdrawAMount;
+  withdrawTotalElement.innerText = newWithdrawTotal;
+
+  const balanceElement = document.getElementById('balance-total');
+  const previousBalanceTotalString = balanceElement.innerText;
+  const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+  const newBalanceTotal = previousBalanceTotal + newWithdrawAMount;
+  balanceElement.innerText = newBalanceTotal;
+});
