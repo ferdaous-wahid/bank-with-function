f443bc30bbdadc5b93f3bc44a04b130b46626739
wahidvai <ferdaouswahid2001@gmail.com>
2022-08-18 21:10:00 +0600

    withdraw upadte

--- js/withdraw.js
+++ js/withdraw.js
@@ -25,4 +25,7 @@
 
 document.getElementById('btn-withdraw').addEventListener('click', function () {
   const newWithdrawAmount = getInputFieldValueById('withdraw-field');
+  const previousWithdrawTotal = getTextElementValueById('withdraw-total');
+  const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+  setTextElementValueById('withdraw-total', newWithdrawTotal);
 });
